39ef6eef6b008f3438a67a90212464a91547ae1f
Illia Bilous <krabbys1111@gmail.com>
2025-04-19 17:56:22 +0300

    solution4

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ kubectl apply -f mysql-secret.yaml
 
 kubectl apply -f statefulSet.yaml
 
-kubectl apply -f todo-db-secret.yml
+kubectl apply -f todo-db-secret.yaml
 
-kubectl apply -f todo-deployment.yml
+kubectl apply -f todo-deployment.yaml
 
 echo "Resources have been deployed successfully."
